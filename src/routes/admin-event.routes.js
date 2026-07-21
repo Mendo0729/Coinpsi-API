a5f2525386b83a9ebabc600b17b6448823047fb9
Abdiel Mendoza <abdielmemdoza2901@gmail.com>
2026-07-20 21:20:40 -0500

    Agrega rutas para cancelar y eliminar eventos

--- src/routes/admin-event.routes.js
+++ src/routes/admin-event.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
 const {
+  cancelEvent,
   createEvent,
+  deleteEvent,
   listEvents
 } = require("../controllers/admin-event.controller");
 const requireAuth = require("../middleware/require-auth");
@@ -11,5 +13,7 @@ const router = express.Router();
 router.use(requireAuth);
 router.get("/", listEvents);
 router.post("/", createEvent);
+router.patch("/:id/cancel", cancelEvent);
+router.delete("/:id", deleteEvent);
 
 module.exports = router;
